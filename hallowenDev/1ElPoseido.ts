fd28a2ac6462113d8e4fd1424a75e992d2498c66
Elkin Arismendy <elk.orion@gmail.com>
2025-10-30 13:36:59 -0500

    Hallowen 1

---
+++ hallowenDev/1ElPoseido.ts
@@ -0,0 +1,53 @@
+function translatePossessed(message: string) {
+  if (message.trim() === "") {
+    return "";
+  }
+  let newmessage = "";
+  let wrd = "";
+  let word = new Intl.Segmenter(undefined, { granularity: "grapheme" });
+
+  let segment = Array.from(word.segment(message), (s) => s.segment);
+
+  for (let i = 0; i < segment.length; i++) {
+    console.log(segment[i] + " " + i);
+    console.log(newmessage);
+    if (segment[i] !== " " || i === segment.length - 1) {
+      wrd = segment[i] + wrd;
+      if (i === segment.length - 1) {
+        newmessage = newmessage + wrd;
+      }
+    } else {
+      newmessage = newmessage + wrd + segment[i];
+      wrd = "";
+    }
+  }
+  return newmessage;
+}
+
+let msm = "          ";
+console.log(translatePossessed(msm));
+
+let segment = Intl.Segmenter;
+
+// Esta siguiente versión falló invertir los emoji y 3 pruebas secretaas:
+
+/*function translatePossessed(message: string): string {
+  let newmessage: string = "";
+  let word: string = "";
+  for (let i = 0; i <= message.length; i++) {
+    if (message[i] === " " || i === message.length) {
+      i === message.length
+        ? (newmessage = newmessage + word)
+        : (newmessage = newmessage + word + message[i]);
+      word = "";
+    } else {
+      word = message[i] + word;
+    }
+  }
+
+  return newmessage;
+}
+
+let msm = "i yojne gnihctaw uoy";
+console.log(translatePossessed(msm));
+*/
