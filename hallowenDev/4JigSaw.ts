758c6d99db426f0261dbec32dd527f4e157d964f
Elkin Arismendy <elk.orion@gmail.com>
2025-11-12 12:31:15 -0500

    4 halloween

--- hallowenDev/4JigSaw.ts
+++ hallowenDev/4JigSaw.ts
@@ -1,5 +1,18 @@
 function changeLock(current: string, target: string): number {
-  // Your code here...
+  const wheel = (actual: number, required: number): number => {
+    let a = actual > required ? actual : required;
+    //(9999,0000)
+    return Math.abs(actual - required) <= 5
+      ? Math.abs(required - actual)
+      : Math.abs(10 - Math.abs(required - actual));
+  };
+  let answer = 0;
+  for (let i = 0; i < 4; i++) {
+    answer = answer + wheel(parseInt(current[i]), parseInt(target[i]));
+    console;
+  }
 
-  return 0;
+  return answer;
 }
+
+console.log(changeLock("9999", "0000"));
